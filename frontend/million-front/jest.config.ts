18ce59eeee69d46f5ae6121a533862259e991759
Camilo Vargas <camilovargasingm@gmail.com>
2025-10-23 21:32:32 -0500

    feat: update tsconfig paths to include core, shared, presentation, and api directories

---
+++ frontend/million-front/jest.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@presentation/(.*)$': '<rootDir>/src/presentation/$1',
+    '^@api/(.*)$': '<rootDir>/src/api/$1',
+  },
+  testMatch: [
+    '**/__test__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__test__/**',
+    '!src/**/__mock__/**',
+  ],
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config);
